2d66eaac96a7e1614a24125bcfb55a147d8ce123
PandLetUsNeverPlay <69433084+PandLetUsNeverPlay@users.noreply.github.com>
2021-10-10 10:07:20 +0200

    Add files via upload

---
+++ src/repair/pand/Main.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace repair\pand;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\TextFormat;
+use pocketmine\utils\Config;
+use pocketmine\Server;
+use repair\pand\commands\RepairCommand;
+
+class Main extends PluginBase {
+
+	public function onEnable() {
+		@mkdir($this->getDataFolder());
+		$this->saveResource("config.yml");
+		$this->saveDefaultConfig();
+		$this->getServer()->getCommandMap()->register($this->getConfig()->get("command"), new RepairCommand($this));
+		$this->getLogger()->info(TextFormat::GREEN . "RepairTheItem was enabled!");
+	}
+	
+	public function onDisable() {
+		$this->getLogger()->info(TextFormat::RED . "RepairTheItem was disabled!");
+	}
+}
